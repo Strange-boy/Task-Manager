0dd343611ffbcad7674d7ca96845bc18b6d2a26a
Joel <joel.panvel@gmail.com>
2025-07-30 15:41:02 +0530

    Revised some basics of signals

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, computed, signal} from '@angular/core';
 import {DUMMY_USERS} from '../dummy-users';
 
 const random = Math.floor(Math.random() * DUMMY_USERS.length);
@@ -10,14 +10,15 @@ const random = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.css'
 })
 export class User {
-  selectedUser = DUMMY_USERS[random];
+  selectedUser = signal(DUMMY_USERS[random]);
+  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar)
 
-  get imagePath(){
-    return 'assets/users/' + this.selectedUser.avatar;
-  }
+  // get imagePath(){
+  //   return 'assets/users/' + this.selectedUser().avatar;
+  // }
 
   onSelectUser(){
     const randomPerson = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser = DUMMY_USERS[randomPerson];
+    this.selectedUser.set(DUMMY_USERS[randomPerson]);
   }
 }
