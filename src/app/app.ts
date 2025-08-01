6eed0294c811e89f993b137aaa0b798aa710587a
Joel <joel.panvel@gmail.com>
2025-08-01 18:10:27 +0530

    Learning about user events in angular

--- src/app/app.ts
+++ src/app/app.ts
@@ -11,4 +11,8 @@ import { DUMMY_USERS } from './dummy-users';
 })
 export class App {
   users = DUMMY_USERS;
+
+  onSelectUser(id: string){
+    console.log("User with the id " + id + " just clicked");
+  }
 }
--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,4 +1,4 @@
-import {Component, computed, Input, input} from '@angular/core';
+import {Component, computed, Input, Output, EventEmitter} from '@angular/core';
 import {DUMMY_USERS} from '../dummy-users';
 
 const random = Math.floor(Math.random() * DUMMY_USERS.length);
@@ -10,19 +10,16 @@ const random = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.css'
 })
 export class User {
-  // @Input({required: true}) avatar!:string;
-  // @Input({required: true}) name!:string;
-  avatar = input.required<string>();
-  name = input.required<string>();
-  imagePath = computed(() => {
-    return 'assets/users/' + this.avatar();
-  })
+  @Input({required: true}) id!: string;
+  @Input({required: true}) avatar!:string;
+  @Input({required: true}) name!:string;
+  @Output() select = new EventEmitter();
 
-  // get imagePath(){
-  //   return 'assets/users/' + this.avatar;
-  // }
+  get imagePath(){
+    return 'assets/users/' + this.avatar;
+  }
 
   onSelectUser(){
-
+    this.select.emit(this.id);
   }
 }
