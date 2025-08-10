6a2ad748170d815d2948ebf41b832b3bd5f9d3b8
Joel <joel.panvel@gmail.com>
2025-08-11 01:40:41 +0530

    Experimenting with two way binding

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,13 +1,20 @@
 import {Component, Output, EventEmitter} from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-new-task',
   templateUrl: './new-task.component.html',
+  imports: [
+    FormsModule
+  ],
   styleUrls: ['./new-task.component.css']
 })
 
 export class NewTaskComponent {
   @Output() cancel = new EventEmitter<void>();
+  enteredTitle  = '';
+  enteredSummary = '';
+  enteredDate = '';
 
   onCancelTask() {
     this.cancel.emit();
